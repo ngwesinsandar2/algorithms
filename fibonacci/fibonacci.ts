854e1f2d5cf35dcf5e15d1cbc7b70e3e567da353
ngwe_sin_sandar <ngwesin.sandar@kbzlife.com>
2024-02-25 23:49:42 +0630

    feat: fibonacci sequence

---
+++ fibonacci/fibonacci.ts
@@ -0,0 +1,23 @@
+/*
+Fibonacci Sequence
+  : an integer in the infinite sequence 1, 1, 2, 3, 5, 8, 13, … of which 
+  the first two terms are 0 and 1 and each succeeding term 
+  is the sum of the two immediately preceding.
+*/
+
+const fibonacci = (n: number) => {
+  const initials = [0, 1];
+  for (let i = 2; i < n; i++) {
+    initials[i] = initials[i - 2] + initials[i - 1]; 
+    // initials[2] = initials[0](0) + initials[1](1) = 1
+  } 
+  return initials;
+};
+
+console.log(fibonacci(2)); //[0, 1]
+console.log(fibonacci(5)); //[0, 1, 1, 2, 3]
+console.log(fibonacci(7)); //[0, 1, 1, 2, 3, 5, 8]
+console.log(fibonacci(10)); //[0, 1,  1,  2,  3, 5, 8, 13, 21, 34]
+
+//BigO = Linear Time: O(n) 
+// -> input size များတာနဲ့(ကြီးတာနဲ့) လုပ်ရတဲ့အကြောင်းအရေအတွက်ပါများလာ များ->များ
